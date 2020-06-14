f0aa7f0bb806c5b95a9095390a1d4a60ca960155
Xiaoyi Liu <lxy878@gmail.com>
2020-06-14 08:55:52 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    print params[:students]
+    print params[:student]
     # student = Student.create(params[:student])
     # redirect_to student_path(student.id)
   end
